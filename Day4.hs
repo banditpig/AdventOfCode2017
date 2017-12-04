dfedf4b0230bef843e0eaa7ed6a4938be5f86558
banditpig <mike_k_houghton@yahoo.co.uk>
2017-12-04 22:44:54 +0000

    after some hacking realised there’s a simple solution

---
+++ Day4.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
+
+module Day4
+where
+
+import           Data.List
+import           Input
+
+allUnique :: Eq a => [a] -> Bool
+allUnique xs = xs == nub xs
+
+main:: IO ()
+main =
+  withData "data/day04.txt" parserDay4 >>= \input -> do
+    putStrLn "Day 4\n-----"
+    -- part 1
+    print $ length $ filter allUnique input
+    --part 2.
+    -- fmap (fmap sort) input => fmap (fmap sort) [[]]
+    -- which runs sort over each string in each row. So then can just apply filter allUnique
+    print $ length $ filter allUnique $ fmap (fmap sort) input
+                                      -- or fmap sort <$> input
+    -- Part 1
+    -- 325
+    -- Part 2
+    -- 119
