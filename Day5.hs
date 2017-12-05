1b66c37b1fa18912e0925c437d56db24f5aa89cf
banditpig <mike_k_houghton@yahoo.co.uk>
2017-12-05 19:12:36 +0000

    part 1

--- Day5.hs
+++ Day5.hs
@@ -1,5 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
--- input = [Int]
--- map =  Map.fromList (zi[pWith input [1..])
--- then runState till locattion > length input
+
+import           Control.Monad.State
+import qualified Data.Map.Strict     as M
+import           Input
+
+makeMap :: [Int] -> M.Map Int Int
+makeMap d = M.fromList $ zip  [1..] d
+
+type JumpState = (M.Map Int Int, Int)
+
+solve :: State JumpState Int
+solve = go 1 where
+    go :: Int -> State JumpState Int
+    go i = do
+        (map, cnt) <- get
+        let step = map M.! i
+            newi = i + step
+            newval = step + 1
+        if newi > M.size map
+            then return (cnt + 1)
+            else do
+                put (M.insert i newval map, cnt + 1)
+                go newi
+
+testMap ::  M.Map Int Int
+testMap = M.fromList [(1,0),(2,3),(3,0),(4,1),(5,-3)]
+
+main :: IO ()
+main =
+    withData "data/day05.txt" parserDay5 >>= \ input -> do
+        putStrLn "Day 5\n-----"
+        -- part 1 = 359348
+        print $ evalState solve (makeMap input, 0)
+
