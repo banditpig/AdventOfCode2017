e45cead1c9218d3d8cbbdd67a72a1ff571070499
banditpig <mike_k_houghton@yahoo.co.uk>
2017-12-05 07:43:35 +0000

    added

---
+++ Day5.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
+-- input = [Int]
+-- map =  Map.fromList (zi[pWith input [1..])
+-- then runState till locattion > length input
