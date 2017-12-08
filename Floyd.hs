091066d095e29041c9f824fa31c72c34946f8f26
banditpig <mike_k_houghton@yahoo.co.uk>
2017-12-08 21:55:45 +0000

    experiment with Floyds cycle algorithm.

--- Floyd.hs
+++ Floyd.hs
@@ -1,20 +1,23 @@
-
+import           Data.List
+import           Data.Maybe
 newtype CF a = CF (Int -> a -> a)
 
 evalCF :: (Eq a) => CF a -> Int -> a -> a
-evalCF (CF f) i x  = f i x
+evalCF (CF f) = f
+ints :: [Int]
+ints  = [1,2,5,6,3,3,3,3,3,3,3,3,2,1]
 
-t :: (Eq a) => CF a -> Int -> a -> a
-t = evalCF
+f :: Int -> [Int] -> [Int]
+f i  xs = if i > length xs then [-1] else [(!!) xs]
 
-h :: (Eq a) =>  CF a -> Int -> a -> a
-h cf i = evalCF cf (i + 1)
-
-checkCycle :: (Eq a) => a -> CF a -> CF a -> Bool
-checkCycle dta t h = go 0 dta t h where
-    go ix dta t h
-     | tv == hv = True
-     | otherwise = go (ix + 1) dta t h where
-        tv = evalCF t ix dta
-        hv = evalCF t (ix + 1) dta
+t = CF f
+h = CF f
+--                      data-tortoise-hare
+checkCycle :: (Eq a) => a -> CF a -> CF a -> (Bool, Int, Int)
+checkCycle = go 0 1  where
+    go tix hix dta t h
+     | tv == hv = (True, tix, hix)
+     | otherwise = go (tix + 1) (hix + 2) dta t h where
+        tv = evalCF t tix dta
+        hv = evalCF h hix dta
 
