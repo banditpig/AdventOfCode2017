fe1dcaf52589069f4dcb33854112ae391a696286
banditpig <mike_k_houghton@yahoo.co.uk>
2017-12-10 20:05:25 +0000

    tidy

--- Day7.hs
+++ Day7.hs
@@ -39,34 +39,25 @@ tknk --- padx - havc
                 xhth
 
 -}
+type WeightMap = M.Map String Int
 
+-- all c
+canBeResolved :: WeightMap -> Tower -> Bool
+canBeResolved mp (Tower _ childNames) = and . map (`M.member` mp)  $ childNames
 
 hasChildren :: Tower -> Bool
 hasChildren (Tower _ ts) = ts /= []
-
                     -- parents -- children
 splitList :: [Tower] -> ([Tower], [Tower])
 splitList ts = (filter hasChildren ts,filter (not . hasChildren) ts)
 
-weightMap :: [Tower] -> M.Map String Int
-weightMap = foldr f M.empty where
-    f (Tower (name, weight) _) = M.insert name weight
-
-towerWeight :: M.Map String Int -> Tower ->  (String, Int)
-towerWeight mp (Tower (name, w) towers) = (name, sumWeight) where
-    sumWeight = foldr (getWeight mp)  w towers
-    getWeight mp (Tower (name, _) _ ) acc  = acc +  M.findWithDefault (-999) name mp
-
-    towerNames  :: [Tower] -> [String]
+towerNames  :: [Tower] -> [String]
 towerNames []                       = []
 towerNames (Tower (name, _) _ : ts) = name : towerNames ts
 
 childNames :: [Tower] -> [String]
 childNames  []                           = []
-childNames (Tower (name, _) towers : ts) = towerNames towers ++ childNames ts
-
-allWeights :: [Tower] -> M.Map String Int -> [(String, Int)]
-allWeights ts mp = foldr (\t ac -> towerWeight mp t : ac) [] ts
+childNames (Tower (name, _) towers : ts) =  towers ++ childNames ts
 
 
 root :: [Tower] -> String
@@ -75,11 +66,18 @@ root ts = res where
     cn = nub . childNames $ ts
     res = head $ (\\) tn  cn
 
+showTower :: [Tower] -> String
+showTower [] = ""
+showTower ( Tower (n, w) _ :ts) = show n ++ " " ++ show w ++ show " || " ++ showTower ts
 main :: IO ()
 main = do
     withData "data/day07.txt" parserDay7 >>= \ towerList -> do
         let (p, c ) = splitList towerList
-        print $ allWeights towerList (weightMap towerList)
+       -- print $ allWeights towerList (weightMap towerList)
         print $ root towerList
 
+        --print $ showTower p
+        -- ugml + (gyxo + ebii + jptl) = 68 + (61 + 61 + 61) = 251
+        -- padx + (pbga + havc + qoyq) = 45 + (66 + 66 + 66) = 243
+        -- fwft + (ktlj + cntj + xhth) = 72 + (57 + 57 + 57) = 243
 
